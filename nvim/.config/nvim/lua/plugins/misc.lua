303bee5cfe2d6d782a6057d928a6b6150a9a0c3d
Blue Zephyr <blue.zephyr.git@gmail.com>
2023-12-19 09:33:36 +0100

    Add keymaps for gitsigns plugin in neovim

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -11,7 +11,33 @@ return {
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
+
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+          if vim.wo.diff then return ']c' end
+          vim.schedule(function() gs.next_hunk() end)
+          return '<Ignore>'
+        end, { expr = true })
+
+        map('n', '[c', function()
+          if vim.wo.diff then return '[c' end
+          vim.schedule(function() gs.prev_hunk() end)
+          return '<Ignore>'
+        end, { expr = true })
+      end
     },
+
+    -- config = function()
+    --     vim.keymap.set('n', '<Leader>gn', "<cmd>lua require('dap').toggle_breakpoint()<cr>",{ desc = 'Debug: run' })
+    -- end
   },
 
 
