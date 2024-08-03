fe418cf47d7077894ae1d8f832d3236f911ed8ed
Blue Zephyr <blue.zephyr.git@gmail.com>
2024-08-03 13:20:51 +0200

    Add mod-g in i3 to toggle fullwidth of single windows

--- i3/.config/i3/scripts/scratchpad-kitty.sh
+++ i3/.config/i3/scripts/scratchpad-kitty.sh
@@ -8,4 +8,7 @@ if  [ -z "$(xdotool search --classname 'scratchpad-kitty')" ]; then
     kitty --class="scratchpad-kitty" tmux & sleep 0.5
 fi
 
-i3-msg "[class=scratchpad-kitty] scratchpad show; move position center"
+i3-msg "[class=scratchpad-kitty] scratchpad show"
+i3-msg "[class=scratchpad-kitty] resize set height 95ppt"
+i3-msg "[class=scratchpad-kitty] resize set width 50ppt"
+i3-msg "[class=scratchpad-kitty] move position center"
---
+++ i3/.config/i3/scripts/toggle-horizontal-gaps.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# This script will toggle the horizontal gaps to 800px for the current window
+i3-msg "gaps horizontal current toggle 800px"
