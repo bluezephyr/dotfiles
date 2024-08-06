8f19684b7eaae271021c1fc37a01ddab634b772a
Blue Zephyr <blue.zephyr.git@gmail.com>
2024-08-06 11:32:28 +0200

    Add overseer plugin to neovim
    Fix problem with commentstring for cpp files. Might be a bug in the
    ts-comments plugin.

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -78,7 +78,8 @@ return {
   -- https://github.com/christoomey/vim-tmux-navigator
   'christoomey/vim-tmux-navigator',
 
-  { -- Theme christoomey/vim-tmux-navigator
+  -- Theme christoomey/vim-tmux-navigator
+  {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000
@@ -94,10 +95,21 @@ return {
     end,
   },
 
-  { -- Plugin for the built in commenting https://github.com/folke/ts-comments.nvim
+  -- Plugin for the built in commenting https://github.com/folke/ts-comments.nvim
+  {
     "folke/ts-comments.nvim",
-    opts = {},
+    opts = {
+      lang = {
+        c = "// %s",
+        cpp = "// %s",
+      },
+    },
     event = "VeryLazy",
     enabled = vim.fn.has("nvim-0.10.0") == 1,
+
+    -- The lang parameter does not seem to work for cpp files. Local fix below
+    config = function()
+      vim.cmd [[ au FileType cpp setlocal commentstring=//\ %s ]]
+    end,
   }
 }
---
+++ nvim/.config/nvim/lua/plugins/overseer.lua
@@ -0,0 +1,7 @@
+-- See https://github.com/stevearc/overseer.nvim
+-- A task runner and job management plugin for Neovim
+-- See `:help lualine.txt`
+return {
+  'stevearc/overseer.nvim',
+  opts = {},
+}
