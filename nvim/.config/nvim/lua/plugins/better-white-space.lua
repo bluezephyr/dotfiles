0c4984e4fc234eea65e60a96d8664e7f5b84d071
Blue Zephyr <blue.zephyr.git@gmail.com>
2024-09-12 10:13:05 +0200

    Add keymaps for StripWhitespaces in nvim

--- nvim/.config/nvim/lua/plugins/better-white-space.lua
+++ nvim/.config/nvim/lua/plugins/better-white-space.lua
@@ -5,6 +5,8 @@ return {
     priority = 1000,
     init = function()
       vim.cmd "let g:better_whitespace_operator=''"
+      vim.keymap.set('n', '<leader>sb',  "<cmd>StripWhitespace<CR>", { desc = 'Strip Whitespaces' })
+      vim.keymap.set('n', '<leader>sB',  "<cmd>StripWhitespaceOnChangedLines<CR>", { desc = 'Strip Whitespaces (changes)' })
     end,
   },
 }
