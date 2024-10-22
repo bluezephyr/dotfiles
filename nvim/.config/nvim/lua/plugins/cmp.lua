fa01e4f72704ee5eb08aae6163d6fa7b576458da
Blue Zephyr <blue.zephyr.git@gmail.com>
2024-10-22 07:31:50 +0200

    Add nvim-cmp configuration

---
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,128 @@
+return {
+    {
+        "onsails/lspkind-nvim",
+        config = function ()
+           require('lspkind').setup()
+        end
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        event = "VeryLazy",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "onsails/lspkind-nvim",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-cmdline",
+            "saadparwaiz1/cmp_luasnip",
+        },
+        config = function()
+            local luasnip = require("luasnip")
+            local cmp = require("cmp")
+            local lspkind = require('lspkind')
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-b>'] = cmp.mapping.scroll_docs( -4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.locally_jumpable(1) then
+                            luasnip.jump(1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                }),
+                formatting = {
+                    format = lspkind.cmp_format({
+                        mode = "symbol_text",
+                        menu = ({
+                            nvim_lsp = '[Lsp]',
+                            luasnip = '[Luasnip]',
+                            buffer = '[File]',
+                            path = '[Path]',
+                        })
+                    }),
+                },
+                sources = {
+                    { name = "nvim_lsp", priority = 1000 },
+                    { name = "luasnip", priority = 15 },
+                    { name = "buffer", priority = 1 },
+                    { name = "path" },
+                },
+                window = {
+                    completion = {
+                        border = {
+                            { "󱐋", "WarningMsg" },
+                            { "─", "Comment" },
+                            { "╮", "Comment" },
+                            { "│", "Comment" },
+                            { "╯", "Comment" },
+                            { "─", "Comment" },
+                            { "╰", "Comment" },
+                            { "│", "Comment" },
+                        },
+                        scrollbar = false,
+                        winblend = 0,
+                    },
+                    documentation = {
+                        border = {
+                            { "󰙎", "DiagnosticHint" },
+                            { "─", "Comment" },
+                            { "╮", "Comment" },
+                            { "│", "Comment" },
+                            { "╯", "Comment" },
+                            { "─", "Comment" },
+                            { "╰", "Comment" },
+                            { "│", "Comment" },
+                        },
+                        scrollbar = false,
+                        winblend = 0,
+                    },
+                }
+            })
+            cmp.setup.cmdline(":", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources {
+                    { name = "path" },
+                    { name = "cmdline" },
+                },
+            })
+            cmp.setup.cmdline({ "/", "?" }, {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = "buffer" },
+                },
+            })
+        end,
+    },
+    {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        config = function ()
+            require("luasnip").setup()
+            require("luasnip.loaders.from_vscode").lazy_load()
+        end
+    },
+    {
+        "rafamadriz/friendly-snippets"
+    },
+}
