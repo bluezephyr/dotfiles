09aa135d1c0d3c467b80261ca56df4cee4433981
Blue Zephyr <blue.zephyr.git@gmail.com>
2025-03-07 07:20:08 +0100

    Nvim: change highlight plugin

--- nvim/.config/nvim/lua/plugins/local-highlight.lua
+++ nvim/.config/nvim/lua/plugins/local-highlight.lua
@@ -1,7 +1,18 @@
 -- https://github.com/tzachar/local-highlight.nvim
 return {
-  'tzachar/local-highlight.nvim',
-  config = function()
-    require('local-highlight').setup()
-  end
+  "tzachar/local-highlight.nvim",
+
+  dependencies = {
+    "folke/snacks.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+
+  opts = {
+    disable_file_types = { "tex", "markdown" },
+    hlgroup = "LocalHighlight",
+    cw_hlgroup = "CWLocalHighlight",
+    animate = {
+      enabled = true,
+    },
+  },
 }
