81f08cb06b6c77eb18e91520930a2ea1d28a288a
Blue Zephyr <blue.zephyr.git@gmail.com>
2026-04-14 07:46:41 +0200

    nvim: Add Index item to dashboard

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -38,7 +38,29 @@ return {
     -- refer to the configuration section below
     -- bigfile = { enabled = true },
     bufdelete = { enabled = true },
-    dashboard = { enabled = true },
+    dashboard = {
+      enabled = true,
+      preset = {
+        -- Defaults to a picker that supports `fzf-lua`, `telescope.nvim` and `mini.pick`
+        ---@type fun(cmd:string, opts:table)|nil
+        pick = nil,
+        -- Used by the `keys` section to show keymaps.
+        -- Set your custom keymaps here.
+        -- When using a function, the `items` argument are the default keymaps.
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "i", desc = "Index", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.expand('~/index'), follow = true, hidden = true, title = 'Index'})" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')", },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')", },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})", },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil, },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+    },
     -- explorer = { enabled = true },
     -- indent = { enabled = true },
     -- input = { enabled = true },
@@ -73,7 +95,7 @@ return {
       "<leader>r",
       function()
         pcall(function()
-          require('snacks').dashboard.open()
+          require("snacks").dashboard.open()
         end)
       end,
       desc = "Open Snacks Dashboard",
